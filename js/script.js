61adef9708300323fa53119249e2947dc780c45f
Egor <94043508+SVENmods@users.noreply.github.com>
2022-08-12 14:42:17 +0300

    made validation

--- js/script.js
+++ js/script.js
@@ -49,3 +49,67 @@ $('a[href*="#"]')
       desableOnInteraction : false,
     },
   });
+
+  function validate(){
+    var check1=document.getElementById('checkbox-cons');
+    var check2=document.getElementById('checkbox-tarif');
+    var check3=document.getElementById('checkbox-mark');
+    // var error=document.getElementById('errorMSG');
+    // var conf=document.getElementById('popup__conf');
+    // var body=document.getElementById('popup__body');
+    if(check1.checked == true){
+      // error.textContent="Вы не подтвердили согласие";
+      console.log('mark1-yes');
+      return false;
+    }
+    if(check2.checked == true){
+      // error.textContent="Вы не подтвердили согласие";
+      console.log('mark2-yes');
+      return false;
+    }
+    if(check3.checked == true){
+      // error.textContent="Вы не подтвердили согласие";
+      console.log('mark3-yes');
+      return false;
+    }
+    else{
+      // body.style.display="none";
+      // conf.style.display="flex";
+      alert('form-clear');
+      return false;
+    }
+}
+
+function getCheckedCheckBoxes() {
+  console.clear();
+  var checkboxes = document.getElementsByClassName('checkbox');
+  var checkboxesChecked = []; // можно в массиве их хранить, если нужно использовать 
+  for (var index = 0; index < checkboxes.length; index++) {
+    if (checkboxes[index].checked) {
+        checkboxesChecked.push(checkboxes[index].value); // положим в массив выбранный
+        console.log(checkboxes[index].value); // делайте что нужно - это для наглядности
+    }
+  }
+  return checkboxesChecked; // для использования в нужном месте
+}
+
+$(document).ready(function(){
+  $('.send-form').submit(function () {
+    var form = $(this);
+    var field = [];
+    form.find('input[data-validate]').each(function () {
+      field.push('input[data-validate]');
+      var value = $(this).val(),
+          line = $(this).closest('.send-form__field');
+      for(var i=0;i<field.length;i++) {
+        if( !value ) {
+          line.addClass('send-form__field-required');
+          setTimeout(function() {
+            line.removeClass('send-form__field-required')
+          }.bind(this),2000);
+          event.preventDefault();
+        }
+      }
+    });
+  });
+});
